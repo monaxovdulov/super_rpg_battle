037e096c0d8528f4759ebd640c1784064506494b
Roman <35300872+monaxovdulov@users.noreply.github.com>
2023-01-21 17:38:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -45,12 +45,24 @@ class Hero:
         self.heal = heal_player
         self.armor = armor_player
 
-    def get_damage(self, enemy):
-        self.hp = self.hp - enemy.power_attack
+    def show_characteristics(self):
+        print(
+            f'{self.name.capitalize()}:\nздоровье - {self.hp}\nсила атаки - {self.power_attack}\nлечение - {self.heal}\nброня - {self.armor}')
+
+    def get_action(self):
+        pass
+
+    def damage_deal(self, damage):
+        self.hp = self.hp - damage
 
     def attack(self, enemy):
         enemy.hp = enemy.hp - self.power_attack
 
+    def turn(self):
+        pass
+
+    def is_dead(self):
+        pass
 
 class Monster:
     def __init__(self, name, hp, power_attack, heal, armor):
@@ -60,20 +72,45 @@ class Monster:
         self.heal = heal
         self.armor = armor
 
-    def get_damage(self, enemy):
-        self.hp = self.hp - enemy.power_attack
+    def show_characteristics(self):
+        print(
+            f'{self.name.capitalize()}:\nздоровье - {self.hp}\nсила атаки - {self.power_attack}\nлечение - {self.heal}\nброня - {self.armor}\n{"-" * 20}\n')
+
+    def get_action(self):
+        pass
+
+    def damage_deal(self, damage):
+        self.hp = self.hp - damage
 
     def attack(self, enemy):
         enemy.hp = enemy.hp - self.power_attack
 
+    def turn(self, action_game):
+        pass
+
+    def is_dead(self):
+        pass
+
+
 
 name_player = input("What your name?")
-player = Hero(name_player, 100, 10, 10, 5)
+player = Hero(name_player, 100, 10, 10, 5)  # создаем объект на основе класса Hero
 print(player.name)
 
-monster = Monster("Ork", 90, 10, 10, 3)
+monster1 = Monster("Ork", 60, 10, 10, 3)  # создаем объект на основе класса Monster
 
-print("Герой актакует монстра")
-player.attack(monster)
-print(monster.hp)
+monster2 = Monster("Goblin", 70, 10, 10, 3)
 
+while True:
+    player.show_characteristics()
+
+    monster1.show_characteristics()
+
+
+    action = player.get_action()
+
+    player.turn(action, target)
+
+    monster1.is_dead()
+
+    break
